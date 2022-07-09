91e8bbfb1512246781a5dc85cb0ff9f95887a8a4
Kasra Mojallal <kasramojallal1@gmail.com>
2022-07-09 18:19:30 +0430

    dtale and orange-3 were added

--- open_AI_gym/cart_pole.py
+++ open_AI_gym/cart_pole.py
@@ -2,7 +2,7 @@ import gym
 env = gym.make('CartPole-v0')
 for i_episode in range(20):
     observation = env.reset()
-    for t in range(100):
+    for t in range(1000):
         env.render()
         print(observation)
         action = env.action_space.sample()
